535be064e16aff16025a125d123daf8049feac52
Alexander Parkhill <alexanderparkhill@gmail.com>
2024-09-09 13:39:55 +1000

    Make cuisineLabelList only display each cuisine once by converting to a Set

--- src/app/components/LabelSearchPanel.js
+++ src/app/components/LabelSearchPanel.js
@@ -4,12 +4,21 @@ import restaurantData from '../assets/ restaurant.json';
 
 export default function LabelSearchPanel({dispatch}){
 
-    const cuisineLabelList = restaurantData.restaurants.map(restaurant => restaurant.cuisine[0])
+    const cuisineLabelList = [...new Set(restaurantData.restaurants.map(restaurant => restaurant.cuisine).flat())];
+
+    function SearchLabel({label}){
+
+        return(
+        <div className="bg-gray-100 p-2 m-3 text-sm rounded-full w-auto h-auto shadow-md">
+            {label}
+        </div>);
+
+    }
 
     return (
-    <div className="w-full h-auto flex items-end">
+    <div className="w-full h-auto flex justify-center">
 
-        {cuisineLabelList.map(label => {return(<p>{label}</p>)})}
+        {cuisineLabelList.map(label => {return(<SearchLabel label={label}/>)})}
         
     </div>
     );
