47cf8890b2c1a6a9a7f099b975218d4f25305111
Alexander Parkhill <alexanderparkhill@gmail.com>
2024-09-09 14:36:59 +1000

    Make label click toggle selectedLabelstate

--- src/app/components/LabelSearchPanel.js
+++ src/app/components/LabelSearchPanel.js
@@ -1,18 +1,25 @@
 'use client'
 
 import restaurantData from '../assets/ restaurant.json';
+import  { toggleSearchLabel } from '../../store/slices/searchSlice'
+import { useSelector } from 'react-redux';
 
 export default function LabelSearchPanel({dispatch}){
 
     const cuisineLabelList = [...new Set(restaurantData.restaurants.map(restaurant => restaurant.cuisine).flat())];
 
-    function handleLabelSelect(){
+    const selectedSearchLabels = useSelector((state) => state.searchData.selectedLabels)
 
+    function handleLabelSelect(e){
+        const label = e.target.getAttribute("data-label");
+        dispatch(toggleSearchLabel(label));
     }
     function SearchLabel({label}){
 
+        const isSelected = selectedSearchLabels.includes(label);
+
         return(
-        <div onClick={handleLabelSelect} className="bg-gray-100 p-2 m-3 text-sm rounded-full w-auto h-auto shadow-md">
+        <div onClick={handleLabelSelect} data-label={label} className={`p-2 m-3 text-sm rounded-full w-auto h-auto shadow-md cursor-pointer text-nowrap transition-colors duration-300 ease-in-out ${isSelected ? 'bg-gray-400 text-white' : 'bg-gray-100 hover:bg-gray-300'}`}>
             {label}
         </div>);
 
