ae94de34637ded9b4bced791f8903cd3bd7a9c54
Alexander Parkhill <alexanderparkhill@gmail.com>
2024-09-09 14:51:45 +1000

    Filter restaurant cards by label

--- src/app/components/RestaurantsPanel.js
+++ src/app/components/RestaurantsPanel.js
@@ -2,16 +2,25 @@
 
 import restaurantData from '../assets/ restaurant.json';
 import RestaurantCard from '../restaurantCard/RestaurantCard';
+import { useSelector } from 'react-redux';
 
 
 export default function RestaurantsPanel({dispatch, selectedRestaurantId}){
 
+    const selectedSearchLabels = useSelector((state) => state.searchData.selectedLabels)
+
     let restaurantArray = restaurantData.restaurants.filter((restaurant) => restaurant.id !== selectedRestaurantId);
+
+    let filteredArray = restaurantArray.filter(restaurant => 
+        restaurant.cuisine.some(cuisineLabel => selectedSearchLabels.includes(cuisineLabel))
+      );
+
+    console.log(filteredArray);
 
 
     return (
     <div className="w-full h-auto flex overflow-x-scroll no-scrollbar items-end">
-        {restaurantArray.map(restaurant => {return( <RestaurantCard restaurant={restaurant} dispatch={dispatch}/> )})}
+        {filteredArray.map(restaurant => {return( <RestaurantCard restaurant={restaurant} dispatch={dispatch}/> )})}
        
     </div>
     );
